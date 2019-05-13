fdf93cb0047745e5ef6f71d928795111ed202b17
admin <admin@DESKTOP-FQOF9AC>
2019-05-13 16:43:56 +0800

    fr

--- DAL/AdministratorDal.cs
+++ DAL/AdministratorDal.cs
@@ -3,12 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Model;
+using Newtonsoft.Json;
 namespace DAL
 {
     //管理员
     public class AdministratorDal
     {
+        
 
+
+
+
+
+
+
+        public List<Administrator> AdministratorsShow(string Name)
+        {
+            string sql = "select *from Administrator where 1=1";
+            if (!string.IsNullOrEmpty(Name))
+            {
+                sql += " and Ads_Name like '%{0}%'" + Name;
+            }
+            var result = DBHelper.GetDataTable(sql);
+            var table = JsonConvert.SerializeObject(result);
+            var list = JsonConvert.DeserializeObject<List<Administrator>>(table);
+            return list;
+        }
     }
 }
---
+++ Model/StaffLogin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+   public class StaffLogin
+    {
+        public int Sl_Id { get; set; }
+        public string Sl_Name { get; set; }
+        public string Sl_Pwd { get; set; }
+        public int Sl_Type { get; set; }
+    }
+}
