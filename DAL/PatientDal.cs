a1ae4c18c9cda8f5fdff556bfbaa686d218dc25f
Summon <@SUMMON>
2019-05-13 17:25:00 +0800

    sqlChange

--- DAL/PatientDal.cs
+++ DAL/PatientDal.cs
@@ -3,11 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Model;
 
 namespace DAL
 {
     //病人信息完善
     public class PatientDal
     {
+        public int PatientAdd(Patient pa)
+        {
+            string sql = "insert into Patient values(" + pa.PatLog_Id + ",'" + pa.Pat_Name + "','" + pa.Pat_Sex + "'," + pa.Pat_Age + ",'" + pa.Pat_Sfz + "','" + pa.Pat_Phone + "')";
+            var da = DBHelper.ExecuteNonQuery(sql);
+            return da;
+        }
+        
     }
 }
