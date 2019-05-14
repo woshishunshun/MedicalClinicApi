df935fc6dc2c52b2e0aacea2a9545ca494480799
Summon <@SUMMON>
2019-05-14 16:16:39 +0800

    change

--- BLL/NurseBll.cs
+++ BLL/NurseBll.cs
@@ -11,6 +11,33 @@ namespace BLL
     public class NurseBll
     {
         NurseDal dal = new NurseDal();
+        /// <summary>
+        /// 病人查询
+        /// </summary>
+        /// <param name="Pat_phone"></param>
+        /// <returns></returns>
+        public List<Patient> PatientShow(string Pat_phone)
+        {
+            return dal.PatientShow(Pat_phone);
+        }
+        /// <summary>
+        /// 病人信息添加
+        /// </summary>
+        /// <param name="pa"></param>
+        /// <returns></returns>
+        public int PatientAdd(Patient pa)
+        {
+            return dal.PatientAdd(pa);
+        }
 
+        /// <summary>
+        /// 挂号
+        /// </summary>
+        /// <param name="re"></param>
+        /// <returns></returns>
+        public int PatientLoginAdd(Registration re)
+        {
+            return dal.PatientLoginAdd(re);
+        }
     }
 }
--- DAL/NurseDal.cs
+++ DAL/NurseDal.cs
@@ -4,10 +4,47 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Model;
+using Newtonsoft.Json;
 
 namespace DAL
 {
     public class NurseDal
     {
+        /// <summary>
+        /// 病人查询
+        /// </summary>
+        /// <param name="Pat_phone"></param>
+        /// <returns></returns>
+        public List<Patient> PatientShow(string Pat_phone)
+        {
+            string sql = "select * from Patient where Pat_Phone = " + Pat_phone;
+            var ds = DBHelper.GetDataTable(sql);
+            var js = JsonConvert.SerializeObject(ds);
+            var jd = JsonConvert.DeserializeObject<List<Patient>>(js);
+            return jd;
+        }
+        /// <summary>
+        /// 病人信息添加
+        /// </summary>
+        /// <param name="pa"></param>
+        /// <returns></returns>
+        public int PatientAdd(Patient pa)
+        {
+            string sql = "insert into Patient values(" + pa.PatLog_Id + ",'" + pa.Pat_Name + "','" + pa.Pat_Sex + "'," + pa.Pat_Age + ",'" + pa.Pat_Sfz + "','" + pa.Pat_Phone + "')";
+            var da = DBHelper.ExecuteNonQuery(sql);
+            return da;
+        }
+        
+        /// <summary>
+        /// 挂号
+        /// </summary>
+        /// <param name="re"></param>
+        /// <returns></returns>
+        public int PatientLoginAdd(Registration re)
+        {
+            string sql = "insert into Patient values(" + re.Pat_Id + ",'" + re.Reg_Time + "'," + re.Adm_Id + "," + re.Doc_Id + "," + re.Reg_Type + ")";
+            var da = DBHelper.ExecuteNonQuery(sql);
+            return da;
+        }
     }
 }
