7922b635b06c1682f68dc6049ff3f9d3777fb175
洛神白绫 <@YUE-ZHENGLING>
2019-05-14 16:37:51 +0800

    病人信息bll

--- DAL/PatientDal.cs
+++ DAL/PatientDal.cs
@@ -10,6 +10,15 @@ namespace DAL
     //病人信息完善
     public class PatientDal
     {
- 
+        /// <summary>
+        /// 完善病人信息
+        /// </summary>
+        /// <param name="patient"></param>
+        /// <returns></returns>
+        public int PatientInfo(Patient patient)
+        {
+            string strSql = string.Format("insert into Patient values({0},'{1}','{2}',{3},'{4}','{5}')",patient.PatLog_Id,patient.Pat_Name,patient.Pat_Sex,patient.Pat_Age,patient.Pat_Sfz,patient.Pat_Phone);
+            return DBHelper.ExecuteNonQuery(strSql);
+        }
     }
 }
