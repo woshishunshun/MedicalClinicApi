7033fa49894c2833d301b7f6ba5477a4a4515f14
顺 齐 <2490475248@qq.com>
2019-05-14 16:50:10 +0800

    addReturnrecord

--- DAL/ReturnrecordDal.cs
+++ DAL/ReturnrecordDal.cs
@@ -3,11 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Model;
 
 namespace DAL
 {
     //回访记录表
     public class ReturnrecordDal
     {
+        /// <summary>
+        /// 添加回访记录表
+        /// </summary>
+        /// <param name="r"></param>
+        /// <returns></returns>
+        public int AddReturnrecord(Returnrecord r)
+        {
+            string sql = string.Format("insert into Returnrecord values('{0}','{1}','{2}','{3}')", r.Pat_Id, r.Rer_Id, r.Rer_Time);
+            return DBHelper.ExecuteNonQuery(sql);
+        }
     }
 }
